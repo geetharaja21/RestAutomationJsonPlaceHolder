4b9dae737ec6202e29f9b51c68a6e7034d545d65
geetharaja <p.r.geetha@gmail.com>
2019-11-25 16:38:10 -0800

    This is my First commit with Path Param.

---
+++ RestAutomationJsonPlaceHolder/src/test/java/com/rest/RestAutomationJsonPlaceHolder/JsonTest.java
@@ -0,0 +1,25 @@
+package com.rest.RestAutomationJsonPlaceHolder;
+
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.path.json.JsonPath;
+
+public class JsonTest {
+
+	@Test
+	public void getUserid() {
+		
+		given()
+		.pathParam("userId", "7")
+		//.queryParam("userId", 7)
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts/{userId}")
+		.then()
+		.statusCode(200)
+		.extract().response().body().prettyPrint();
+	}
+}
